21790b4d410c92082731074205ec8d86a7593533
Zacky <zacky-kh@live.com>
2019-10-15 10:28:35 +0200

    Databse connection

---
+++ DbHandlar/dbParser.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+class mongoose {
+
+	constructor (){
+		this.uri = 'mongodb://<dbuser>:<dbpassword>@ds135068.mlab.com:35068/task25' 
+	}
+
+	connect(){
+		mongoose.connect(this.uri);
+	}
+
+		let db = mongoose.connection
+        db.on('connected', function () {
+            console.log('Database connected')
+          })
+        db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
+}
