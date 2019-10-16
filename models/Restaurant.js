962277e455ad5ed2cf627987a574667fae691694
Zacky <zacky-kh@live.com>
2019-10-16 09:13:42 +0200

    forgien keys for collections works

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-const Schema  = mongoose.Schema;
+const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 let restaurantSchema = new mongoose.Schema({
+    id : {type:Number  , unique :true},
     name: { type: String, required: true },
     address: { type: String, required: true },
     description: { type: String, required: true },
     tags : { type: Array, required: true },
-    owner : { type: Schema.Types.ObjectId, ref: 'users' }
- });
+    owner : { type: Number, ref: 'users' }
+ },{timestamps:true});
 
+ restaurantSchema.plugin(AutoIncrement, {inc_field: 'id'});
 let Restaurant = mongoose.model('restaurants', restaurantSchema);
 module.exports = Restaurant;
--- models/Review.js
+++ models/Review.js
@@ -1,13 +1,10 @@
 const mongoose = require('mongoose')
-const Schema  = mongoose.Schema;
-const AutoIncrement = require('mongoose-sequence')(mongoose);
 
 let reviewSchema = new mongoose.Schema({
-   _id : Number,
-   //userID: { type: Number, ref: 'users', required: true },
-  // restaurantID: { type: Number, ref: 'restaurants', required: true },
+   userID: { type: Number, ref: 'users', required: true },
+   restaurantID: { type: Number, ref: 'restaurants', required: true },
    rating: { type: mongoose.Types.Decimal128, required: true },
    text : { type: String, required: true }
-},{_id : false})
+},{timestamps:true})
 let Review = mongoose.model('reviews', reviewSchema)
 module.exports = Review
